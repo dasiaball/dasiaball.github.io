e3bc05c0ee78c2d8f8b58083891af72fb76de063
dasia ball <dasia.ball@sciacademy.org>
2023-12-04 20:35:56 +0000

    add index.html and css files

--- projects/circularity/js/init.js
+++ projects/circularity/js/init.js
@@ -33,6 +33,8 @@ var init = function (window) {
 
             //set motion properties
             physikz.addRandomVelocity(circle, canvas);
+            circles.push(circle);
+            view.addChild(circle);
 
             //add the circle to circles array, add to view 
             circles.push(circle);
@@ -59,10 +61,19 @@ var init = function (window) {
         */
         function update() {
             // TODO 4 : Update the circle's position //
-            drawCircle()
+                //physikz.updatePosition(circles[0]);
+                //physikz.updatePosition(circles[1]);
+                //physikz.updatePosition(circles[2]);
+                //physikz.updatePosition(circles[3]);
+                //physikz.updatePosition(circles[4]);
+              }
             
             // TODO 5 / 10 : Call game.checkCirclePosition() on your circles.
-           
+            //game.checkCirclePosition(circles[0]);
+            //game.checkCirclePosition(circles[1]);
+            //game.checkCirclePosition(circles[2]);
+            //game.checkCirclePosition(circles[3]);
+            //game.checkCirclePosition(circles[4]);
 
             // TODO 9 : Iterate over the array
            
